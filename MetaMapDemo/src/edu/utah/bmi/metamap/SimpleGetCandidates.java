b24cce7d7a47fd5cb36f83e903b15d20eaf5fea9
brokenjade3000 <brochor3000@gmail.com>
2014-06-30 16:13:07 -0600

    add

--- MetaMapDemo/src/edu/utah/bmi/metamap/SimpleGetCandidates.java
+++ MetaMapDemo/src/edu/utah/bmi/metamap/SimpleGetCandidates.java
@@ -8,31 +8,46 @@ import gov.nih.nlm.nls.metamap.Result;
 import gov.nih.nlm.nls.metamap.Utterance;
 
 import java.util.List;
+import java.util.TreeMap;
 
-public class SimpleRun {
+import gov.nih.nlm.nls.metamap.Position;
+
+/**
+ * 
+ * This demo code is adopted from MetaMap official API documentation
+ * 
+ * http://metamap.nlm.nih.gov/Docs/README_javaapi.html
+ * 
+ * @author Jianlin Shi
+ *
+ */
+public class SimpleGetCandidates {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MetaMapApi api=new MetaMapApiImpl();
-		String query=args[0];
+		
+		
+		String query="We did obtain a chest CT.";
+//		initiate a semantic type interpreter
+//		STYInterpretor sytInterpretor=new STYInterpretor("data/SemanticTypes_2013AA.txt");
+		
+//		 Instantiating the API
+		MetaMapApi api=new MetaMapApiImpl();		
 		try{
-			//let me try moving the mmap parse inside the try block to see if I can catch those mysterious mmap errors
-			//OK, there was text, do something with it. First, stuff it through the 'map:
-			System.out.println("$"+args+"$");
-			List<Result> resultList = api.processCitationsFromString(query);
-			
-			//They do get caught, but it seems that nothing ever works again after that.
-			
+//			Performing a query 	
+			List<Result> resultList = api.processCitationsFromString(query);				
 			for(Result result: resultList) {
+//				produces a list of the utterances: 
 				for (Utterance utterance: result.getUtteranceList()) {
 					System.out.println("Utterance:");
 					System.out.println(" Id: " + utterance.getId());
 					System.out.println(" Utterance text: " + utterance.getString());
 					System.out.println(" Position: " + utterance.getPosition());
-					for(PCM pcm:utterance.getPCMList()) {
+//					To get the list of phrases, candidates, and mappings associated with an utterance 
+					for(PCM pcm:utterance.getPCMList()) {						
 						System.out.println("Phrase:");
 						System.out.println(" text: " + pcm.getPhrase().getPhraseText());						
 						System.out.println("Candidates:");
+//						get the candidate list 
 				          for (Ev ev: pcm.getCandidateList()) {
 				            System.out.println(" Candidate:");
 				            System.out.println("  Score: " + ev.getScore());
@@ -41,12 +56,20 @@ public class SimpleRun {
 				            System.out.println("  Preferred Name: " + ev.getPreferredName());
 				            System.out.println("  Matched Words: " + ev.getMatchedWords());
 				            System.out.println("  Semantic Types: " + ev.getSemanticTypes());
+//				            System.out.println("  Semantic Types: " + sytInterpretor.getMeanings(ev.getSemanticTypes()));
 				            System.out.println("  Sources: " + ev.getSources());
-				            System.out.println("  Span position infor: " + ev.getPositionalInfo());
+//				            this is the span information 
+				            List<Position> positions=ev.getPositionalInfo();
+				            System.out.println("  Span position infor: " + positions);				            
+//				            how to get the begin and end of span
+				            for(Position p:positions){
+				            	System.out.println("\tspan begin:\t"+p.getX());
+				            	System.out.println("\tspan end:\t"+(p.getX()+p.getY()));
+				            }
 				          }
-					}// end PCM
-				}//end Utterance
-			} //end Result
+					}
+				}
+			} 
 
 		} catch (Exception e) {
 			e.printStackTrace();
